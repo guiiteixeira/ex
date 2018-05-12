b38cc221017a71681f9b61bbde7cf61bbcdf4c56
Guilherme <guisebastian15@gmail.com>
2018-05-12 13:44:57 -0300

    versao 1.0

---
+++ A.java
@@ -0,0 +1,22 @@
+public class A{
+
+	private int a;
+	private int b;
+
+	public void setA(int a){
+		this.a = a;
+	}
+
+	public void setB(int b){
+		this.b = b;
+	}
+
+	public int getA(){
+		return this.a;
+	}
+
+	public int getB(){
+		return this.b;
+	}
+
+}
