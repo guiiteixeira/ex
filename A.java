ce602287d2d52d1708927310896105001d22c1fe
Guilherme <guisebastian15@gmail.com>
2018-05-12 13:57:39 -0300

    Atributo String A3

--- A.java
+++ A.java
@@ -2,6 +2,7 @@ public class A{
 
 	private int a;
 	private int b;
+	private String A3;
 
 	public void setA(int a){
 		this.a = a;
@@ -17,6 +18,14 @@ public class A{
 
 	public int getB(){
 		return this.b;
+	}
+
+	public void setA3(String A3){
+		this.A3 = A3;
+	}
+
+	public int getA3(){
+		return this.A3;
 	}
 
 }
